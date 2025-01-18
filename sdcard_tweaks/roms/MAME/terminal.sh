111b1297a2781c21747029a14936c0915848627a
nk64 <24872528+nk64@users.noreply.github.com>
2025-01-18 22:47:01 +0930

    Added terminal User Script and binary (thanks VkCommodore - https://github.com/nk64/GT4286/discussions/29)

---
+++ sdcard_tweaks/roms/MAME/terminal.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#region Boilerplate Script Header
+this_file=${0}
+output_dir="${GT4286UTIL_HOME}/output"
+mkdir -p "${output_dir}"
+logfile="${output_dir}/log.txt"; log () { echo "${this_file}: ${1}" >> "${logfile}" && /bin/sync; }
+cat /dev/urandom > /dev/fb0
+#endregion Boilerplate Script Header
+
+log "Test sdl terminal"
+
+byobb="${GT4286UTIL_HOME}/bin/busybox_arm"
+
+if [ ! -f "${byobb}" ]; then
+    log "The built-in busybox doesn't have this command."
+    log "Try: https://github.com/EXALAB/Busybox-static/tree/main/busybox_arm"
+    log "Put it in: ${GT4286UTIL_HOME}/bin/busybox_arm"
+else
+    log "BYO Busybox found: ${byobb}"
+
+    st="${GT4286UTIL_HOME}/bin/st"
+    ${st}
+fi
+
+
